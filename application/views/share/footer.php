eb8fc8406db91a7c0c8962de957bb3bbfd6b5fa5
teamlab-lai <lai@team-lab.com>
2016-06-15 17:43:09 +0900

    del all file in pf-page

--- application/views/share/footer.php
+++
@@ -1,8 +0,0 @@
-      <footer class="site-footer">
-          <div class="text-center">
-              2014 - Lars.is
-              <a href="#top" class="go-top">
-                  <i class="fa fa-angle-up"></i>
-              </a>
-          </div>
-      </footer>
