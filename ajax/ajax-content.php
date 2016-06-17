2e37b94f20ba71181f552dac505b036c8cc079ef
teamlab-lai <lai@team-lab.com>
2016-06-17 17:48:04 +0900

    ajax test

--- ajax/ajax-content.php
+++ ajax/ajax-content.php
@@ -306,12 +306,12 @@ function genImgDOM($img_list){
 
 
 
-			$dom .= "<div class=\"viewport detail-style\">
-						<a href=\"javascript:void(0);\">
-							<span class=\"dark-background db-s-size\">".$short_name."</span>
-							<img class=\"img-circle xxs-size\" src=\"images/skills/".$pic_name."\"/>
+			$dom .= '<div class="viewport detail-style">
+						<a href="javascript:void(0);">
+							<span class="dark-background db-s-size">'.$short_name.'</span>
+							<img class="img-circle xxs-size" src="images/skills/'.$pic_name.'"/>
 						</a>
-					</div>";
+					</div>';
 		}
 
 		return null;
